1dedd971160273647453cd405ab32d65c0b6a980
tihoph <tihoph@noreply.git.audivir.de>
2026-03-17 23:35:02 +0100

    Add packages/go.sh

---
+++ packages/go.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+os="$(uname)"
+if [ "$os" = "Linux" ]; then
+    os="linux"
+elif [ "$os" = "Darwin" ]; then
+    os="darwin"
+else
+    echo "Unsupported OS: $os"
+    exit 1
+fi
+
+arch="$(uname -m)"
+if [ "$arch" = "x86_64" ] || [ "$arch" = "amd64" ];
+    arch="amd64"
+elif [ "$arch" = "arm64" ] || [ "$arch" = "aarch64" ];
+    arch="arm64"
+else
+    echo "Unsupported architecture: $arch" >&2
+    exit 1
+fi
+
+latest_go="$(curl -sL "https://go.dev/VERSION?m=text" | head -n 1)"
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT INT TERM
+curl -sL "https://go.dev/dl/$latest_go.$os-$arch.tar.gz" | tar -xzC "$tmpdir"
+mv "$tmpdir/go" "$XDG_DATA_HOME/golang" || return 1
+rm -rf "$tmpdir"
+trap - EXIT INT TERM
