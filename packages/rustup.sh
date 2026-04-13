f7b7cfad7d0a1a169be85b8e6bd3da7286f18930
Tim Hörmann <pypi@audivir.de>
2026-04-13 11:28:16 +0200

    pre-commit lint

--- packages/rustup.sh
+++ packages/rustup.sh
@@ -23,9 +23,9 @@ fetch() {
 # install the most recent version
 install() {
     curl --fail-with-body -L "https://sh.rustup.rs" | sh -s -- \
-  	    --default-toolchain "nightly-2026-01-28" \
-  	    --no-update-default-toolchain \
-  	    --no-modify-path -y
+        --default-toolchain "nightly-2026-01-28" \
+        --no-update-default-toolchain \
+        --no-modify-path -y
 }
 
 # uninstall the installed package
